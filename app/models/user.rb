75eb555be706ea1639cedd332e121524cb5a4d4b
Srdjan Margetic <srdjan1983@live.com>
2022-07-05 00:08:05 -0400

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
                 length: { minimum: 3, maximum: 25 }
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    
+
     validates :email, presence: true, 
-                uniqueness: { case_sensitive: false }, 
-                length: { maximum: 105 }
+                uniqueness: { case_sensitive: false },
+                length: { maximum: 105 },
+                format: { with: VALID_EMAIL_REGEX }
+
     has_many :blogs
 end
