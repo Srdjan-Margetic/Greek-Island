bffe26d01a2f3a3c76646478fea560c07446082a
Srdjan Margetic <srdjan1983@live.com>
2022-07-04 23:36:35 -0400

    Create User, add user to the blog by migration and add has_many/belogs_to to the models

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 6, maximum: 300 }
+  belongs_to :user
 end
